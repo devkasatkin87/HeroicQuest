e9b3fed37fe4f4352128341e26afc429ff8c51cf
Serhii Kasatkin <devkasatkin87@gmail.com>
2024-01-07 17:12:04 +0200

    Added true gitignore

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import com.gamecontrol.devkasatkin.Game;
 public class Main {
     public static void main(String[] args) {
         Game game = Game.getInstance();
-        System.out.println(game);
         game.run();
     }
 }
